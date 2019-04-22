e43e1a6992920209f39c10b7dbf83774e6ab3388
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-04-22 18:37:44 +0200

    Add object in cache and build docs

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,7 +12,8 @@ const seriesListEl = document.querySelector('.series__list');
 const inputEl = document.querySelector('.search');
 const buttonEl = document.querySelector('.btn');
 const favouriteListEl = document.querySelector('.favourites__list');
-const favouritesArray = [];
+let favouritesArray =  JSON.parse(localStorage.getItem('favouritesArray'));
+printFavourites();
 //Añadir listener de click a button
 //En la función añadir el value del input a la URL de búsqueda.
 //Recoger título e imagen de la info recibida.
@@ -43,6 +44,21 @@ function printerSeries(data) {
         seriesListEl.appendChild(showLi);
     }
 }
+function printFavourites(){
+for (let favourite of favouritesArray) {
+    const favouriteName = favourite.name;
+    const showLi = document.createElement('li');
+    const showNameEl = document.createElement('h3');
+    const showImageEl = document.createElement('img');
+    const showNameContent = document.createTextNode(favouriteName);
+    const favouriteImage = favourite.image;
+    showImageEl.src = favouriteImage;
+    showNameEl.appendChild(showNameContent);
+    showLi.appendChild(showImageEl);
+    showLi.appendChild(showNameEl);
+    favouriteListEl.appendChild(showLi);
+}
+}
 function likeShow() {
     const showList = document.querySelectorAll('.show__element');
     for (let show of showList)
@@ -58,20 +74,10 @@ function likeShow() {
         favouritesArray.push(obj);
         console.log(favouritesArray);
         favouriteListEl.innerHTML = '';
-        for (let favourite of favouritesArray) {
-            const favouriteName = favourite.name;
-            const showLi = document.createElement('li');
-            const showNameEl = document.createElement('h3');
-            const showImageEl = document.createElement('img');
-            const showNameContent = document.createTextNode(favouriteName);
-            const favouriteImage = favourite.image;
-            showImageEl.src = favouriteImage;
-            showNameEl.appendChild(showNameContent);
-            showLi.appendChild(showImageEl);
-            showLi.appendChild(showNameEl);
-            favouriteListEl.appendChild(showLi);
-        }
+        printFavourites();
         
+        
+        localStorage.setItem('favouritesArray', JSON.stringify(favouritesArray));
     }
 
 }
