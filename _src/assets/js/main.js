940f57541550e5323710f2d406ab7bca925bf499
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-04-23 19:34:49 +0200

    Add better styles, build docs

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,38 +6,9 @@ const buttonEl = document.querySelector('.btn');
 const favouriteListEl = document.querySelector('.favourites__list');
 const savedFavourites = JSON.parse(localStorage.getItem('favourites'));
 let favouritesArray = savedFavourites || [];
-const deleteButtonEl = document.querySelector('.btn__delete'); 
+const deleteButtonEl = document.querySelector('.btn__delete');
 printFavourites();
 
-
-const printerSeries = data => {
-    for (let showEl of data) {
-        const seriesEl = showEl.show;
-        const showName = seriesEl.name;
-        const showId = seriesEl.id;
-        const showLi = document.createElement('li');
-        const showNameEl = document.createElement('h3');
-        const showImageEl = document.createElement('img');
-        const showNameContent = document.createTextNode(showName);
-        const showImageGlobal = seriesEl.image;
-        if (!showImageGlobal) {
-            showImageEl.src = "https://via.placeholder.com/210x295/444444/ffffff/?text=" + showName + "";
-        }
-        else {
-            const showImage = showImageGlobal.medium;
-            showImageEl.src = showImage;
-        }
-        showImageEl.alt = 'Cartel de la serie ' + showName;
-        showNameEl.classList.add('show-title');
-        showLi.classList.add('show__element');
-        showLi.classList.add('no-favourite');
-        showLi.setAttribute("id", showId);
-        showNameEl.appendChild(showNameContent);
-        showLi.appendChild(showImageEl);
-        showLi.appendChild(showNameEl);
-        seriesListEl.appendChild(showLi);
-    }
-}
 function printFavourites() {
     for (let favourite of favouritesArray) {
         const favouriteName = favourite.name;
@@ -58,13 +29,41 @@ function printFavourites() {
         favouriteListEl.appendChild(showLi);
     }
 }
+const printerSeries = data => {
+    for (let showEl of data) {
+        const seriesEl = showEl.show;
+        const showName = seriesEl.name;
+        const showId = seriesEl.id;
+        const showLi = document.createElement('li');
+        const showNameEl = document.createElement('h3');
+        const showImageEl = document.createElement('img');
+        const showNameContent = document.createTextNode(showName);
+        const showImageGlobal = seriesEl.image;
+        showNameEl.classList.add('show-title');
+        showLi.classList.add('show__element');
+        showLi.classList.add('no-favourite');
+        showLi.setAttribute("id", showId);
+        showNameEl.appendChild(showNameContent);
+        showLi.appendChild(showImageEl);
+        showLi.appendChild(showNameEl);
+        seriesListEl.appendChild(showLi);
+        showImageEl.alt = 'Cartel de la serie ' + showName;
+        if (!showImageGlobal) {
+            showImageEl.src = "https://via.placeholder.com/210x295/444444/ffffff/?text=" + showName + "";
+        }
+        else {
+            const showImage = showImageGlobal.medium;
+            showImageEl.src = showImage;
+        }
+    }
+};
 const containsObject = (obj, array) => {
     for (let i = 0; i < array.length; i++) {
         if (obj.id === array[i].id) {
             return true;
         }
     }
-}
+};
 const likeThisShow = event => {
     const showElement = event.currentTarget;
     const showImage = showElement.childNodes[0].src;
@@ -83,12 +82,12 @@ const likeThisShow = event => {
         localStorage.setItem('favourites', JSON.stringify(favouritesArray));
         printFavourites();
     }
-}
+};
 const likeShow = () => {
     const showList = document.querySelectorAll('.no-favourite');
     for (let show of showList)
         show.addEventListener('click', likeThisShow);
-}
+};
 const searchSeries = () => {
     seriesListEl.innerHTML = '';
     const inputValue = inputEl.value;
@@ -100,7 +99,7 @@ const searchSeries = () => {
             printerSeries(data);
             likeShow();
         });
-}
+};
 const removeAllFavourites = () => {
     localStorage.removeItem('favourites');
     favouriteListEl.innerHTML = '';
@@ -109,7 +108,7 @@ const removeAllFavourites = () => {
     for (let child of children) {
         child.classList.remove('like');
     }
-}
+};
 buttonEl.addEventListener('click', searchSeries);
 deleteButtonEl.addEventListener('click', removeAllFavourites);
 
